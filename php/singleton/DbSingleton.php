2c5635a30e3b45bb9207c276a1be665d421d7d8a
lai2-24 <lai2-24@itismeucci.com>
2026-03-09 10:26:37 +0100

    added crud operations

---
+++ php/singleton/DbSingleton.php
@@ -0,0 +1,13 @@
+<?php
+class DbSingleton {
+    private static object $instance;
+
+    public static function getInstance() {
+        if (!isset(self::$instance)) {
+            self::$instance = new MySQLi('p:my_mariadb', 'root', 'ciccio', 'scuola');
+        }
+        
+        return self::$instance;
+    }
+}
+?>
