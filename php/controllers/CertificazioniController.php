aece600ea7ed0b736e9196a0389185a4a6a149da
GavrisFlavius07 <gavris.flavius@itismeucci.com>
2026-04-13 10:19:52 +0200

    added certificazioniController.php

--- php/controllers/CertificazioniController.php
+++ php/controllers/CertificazioniController.php
@@ -4,11 +4,11 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 
 require_once __DIR__ . '/../singleton/DbSingleton.php';
 
-class AlunniController
+class CertificazioniController
 {
   public function index(Request $request, Response $response, $args){
     $mysqli = DbSingleton::getInstance();
-    $result = $mysqli->query("SELECT * FROM alunni");
+    $result = $mysqli->query("SELECT * FROM certificazioni");
     $rows = $result->fetch_all(MYSQLI_ASSOC);
 
     $response->getBody()->write(json_encode($rows));
@@ -17,7 +17,7 @@ class AlunniController
 
   public function show(Request $request, Response $response, $args){
     $mysqli = DbSingleton::getInstance();
-    $stmt = $mysqli->prepare("SELECT * FROM alunni WHERE id = ?");
+    $stmt = $mysqli->prepare("SELECT * FROM certificazioni WHERE id = ?");
     $stmt->bind_param("s", $args["id"]);
     $stmt->execute();
 
@@ -32,8 +32,8 @@ class AlunniController
     $data = json_decode((string)$request->getBody(), true);
 
     $mysqli = DbSingleton::getInstance();
-    $stmt = $mysqli->prepare("INSERT INTO alunni (nome, cognome) VALUES (?, ?)");
-    $stmt->bind_param("ss", $data['nome'], $data['cognome']);
+    $stmt = $mysqli->prepare("INSERT INTO certificazioni (alunno_id, titolo, votazione, ente) VALUES (?, ?,?,?)");
+    $stmt->bind_param("ssss", $data['alunno_id'], $data['titolo'], $data['votazione'], $data['ente']);
     $stmt->execute();
 
     $response->getBody()->write("CREATED");
@@ -44,8 +44,8 @@ class AlunniController
     $data = json_decode((string)$request->getBody(), true);
 
     $mysqli = DbSingleton::getInstance();
-    $stmt = $mysqli->prepare("UPDATE alunni SET nome = ?, cognome = ? WHERE id = ?");
-    $stmt->bind_param("sss", $data['nome'], $data['cognome'], $args['id']);
+    $stmt = $mysqli->prepare("UPDATE certificazioni SET titolo = ?, ente = ? WHERE id = ?");
+    $stmt->bind_param("sss", $data['certificazioni'], $data['titolo'], $args['id']);
     $stmt->execute();
 
     $response->getBody()->write("UPDATED");
@@ -54,7 +54,7 @@ class AlunniController
 
   public function destroy(Request $request, Response $response, array $args){
     $mysqli = DbSingleton::getInstance();
-    $stmt = $mysqli->prepare("DELETE FROM alunni WHERE id = ?");
+    $stmt = $mysqli->prepare("DELETE FROM certificazioni WHERE id = ?");
     $stmt->bind_param("s", $args['id']);
     $stmt->execute();
 
